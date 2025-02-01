9de4c0424352197a8cec2e05bb79268d358e27ad
Sarang1995 <sarang.suk04@gmail.com>
2025-02-01 16:02:00 +0530

    create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+data = pd.read_csv('data.csv')
+
+st.title("Welcome to the Restaurant Recommendation System")
+st.write("Here you will get a recommendation of restaurant and recommendations will be based on Price and Cuisine")
+
+st.write("I used cosine similarity technique to find the similarity between restaurants")
+
+name = st.selectbox("Select the Restaurant you like", data['Name'].unique().tolist())
+
+recs = (data[['Name',name]].sort_values(by=name, ascending=False)
+        .head(10))
+
+st.write("Here are your top 10 recommendation based on your selected restaurant")
+
+st.dataframe(recs)
