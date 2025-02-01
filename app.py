5de8e30d97f0d619ee3d8c63f0554626e729d0f3
Sarang1995 <sarang.suk04@gmail.com>
2025-02-01 16:10:58 +0530

    create tabs

--- app.py
+++ app.py
@@ -3,16 +3,25 @@ import pandas as pd
 
 data = pd.read_csv('data.csv')
 
-st.title("Welcome to the Restaurant Recommendation System")
-st.write("Here you will get a recommendation of restaurant and recommendations will be based on Price and Cuisine")
+tab1, tab2 = st.tabs(["Content-Based Filtering", "Collaborative Filtering"])
 
-st.write("I used cosine similarity technique to find the similarity between restaurants")
+with tab1:
 
-name = st.selectbox("Select the Restaurant you like", data['Name'].unique().tolist())
+    st.title("Welcome to the Restaurant Recommendation System")
+    st.write("Here you will get a recommendation of restaurant and recommendations will be based on Price and Cuisine")
 
-recs = (data[['Name',name]].sort_values(by=name, ascending=False)
-        .head(10))
+    st.write("I used cosine similarity technique to find the similarity between restaurants")
 
-st.write("Here are your top 10 recommendation based on your selected restaurant")
+    name = st.selectbox("Select the Restaurant you like", data['Name'].unique().tolist())
 
-st.dataframe(recs)
+    recs = (data[['Name',name]].sort_values(by=name, ascending=False)
+            .reset_index()
+            .head(10))
+
+    st.write("Here are your top 10 recommendation based on your selected restaurant")
+
+    st.dataframe(recs[["Name"]])
+
+with tab2:
+
+    st.subheader("Collaborative Filtering: Recommend Restaurants to a New User")
